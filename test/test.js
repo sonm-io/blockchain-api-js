3b399993695dd3f42783d8804fb38ed002da55a1
dmitrypisanko <dmitry.pisanko@gmail.com>
2017-11-15 13:49:42 +0300

    Change provider, export TxResult and update Votes logic

--- test/test.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const getPrivateKey = require('../src/utils/read-private-key-json');
-const testWallet = require('./data/Vasya_11111111.json');
-
-console.log(getPrivateKey(testWallet, '11111111'));
